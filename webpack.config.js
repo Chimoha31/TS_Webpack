54cdf1148669e0dca3432ba809821ed640ad79cf
Chimoha31 <mokochii1108@gmail.com>
2022-03-29 15:52:35 -0700

    setting webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,22 @@ module.exports = {
     // importした時の拡張子を省いてもokなように記載。import 〇〇 from "./index"だけでokと言うことindex.jsとかしなくて良い。
     extensions: [".ts", ".js"],
   },
-
+  // local serverを立ち上げる時に何を見にいくか、どこをweb serverとして立ち上げるか
+  // つまり、tsをコンパイルし終わったjsを見に行ってくださいね、という事＝jsからindex.htmlを見てください。
+  devServer: {
+    static: {
+      directory: `${__dirname}/dist`,
+    },
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子.tsをコンパイルしている
+        test: /\.ts$/,
+        // installしたloaderに対し、.tsと付いている拡張子をts-loaderを使ってコンパイルして下さいと命令
+        loader: "ts-loader",
+      },
+    ],
+  },
 };
